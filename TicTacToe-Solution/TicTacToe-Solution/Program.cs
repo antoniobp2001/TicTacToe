712edda7b0ac5106c9d2b92010dae054562da69c
António Pereira <antoniobpereira2001@gmail.com>
2024-01-08 17:48:26 +0000

    Update Program
    Created the board

--- TicTacToe-Solution/TicTacToe-Solution/Program.cs
+++ TicTacToe-Solution/TicTacToe-Solution/Program.cs
@@ -1,10 +1,67 @@
 ﻿using System;
 
-class Program { 
-    public static void Main(string[] args)
+namespace TicTacToe_Solution
+{
+    class Program
     {
-        Console.WriteLine("Welcome to the Tic Tac Toe Game");
+        static char[,] board = new char[3, 3];
+        public static void InitiatilizeBoard()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    board[i, j] = '-';
+                }
+            }
+
+            Console.WriteLine("  0 1 2");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write(i + " ");
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(board[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        public static void DisplayBoard()
+        {
+            Console.WriteLine("  0 1 2");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write(i + " ");
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(board[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        public static void Main(string[] args)
+        {
+            bool gameFinish = false;
+            char currentPlayer = 'X';
+
+            InitiatilizeBoard();
+
+            Console.WriteLine("Welcome to the Tic Tac Toe Game");
+
+            while (!gameFinish)
+            {
+                DisplayBoard();
+
+                Console.WriteLine($"Player {currentPlayer} turn");
 
 
+            }
+
+
+        }
     }
 }
